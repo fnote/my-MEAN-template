5cab31f8b89b26204cee69959bae3caca0219078
sithijat <t.sithija@mubasher.net>
2017-10-31 15:45:33 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  // Define a users property to hold our user data
+  users: Array<any>;
+
+  // Create an instance of the DataService through dependency injection
+  constructor(private _dataService: DataService) {
+
+    // Access the Data Service's getUsers() method we defined
+    this._dataService.getUsers()
+      .subscribe(res => this.users = res);
+  }
 }
